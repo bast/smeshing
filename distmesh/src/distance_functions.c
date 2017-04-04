31eda49ee23f4532d3e460ee9a38652d961a2de2
Radovan Bast <bast@users.noreply.github.com>
2017-04-04 16:09:04 +0200

    reorder

--- distmesh/src/distance_functions.c
+++ distmesh/src/distance_functions.c
@@ -17,13 +17,21 @@ double dsegment(double x0,double y0,double p1x,double p1y,double p2x,double p2y)
   double w[2] = {x0 - p1x, y0 - p1y};
 
   double c1 = v[0]*w[0] + v[1]*w[1];
-  double c2 = v[0]*v[0] + v[1]*v[1];
 
   if (c1 <= 0.0)
+  {
     return length(x0 - p1x, y0 - p1y);
-  else if (c1 >= c2)
+  }
+
+  double c2 = v[0]*v[0] + v[1]*v[1];
+
+  if (c1 >= c2)
+  {
     return length(x0 - p2x, y0 - p2y);
+  }
   else
+  {
     return length(x0 - (p1x + c1/c2*v[0]),
                   y0 - (p1y + c1/c2*v[1]));
+  }
 }
