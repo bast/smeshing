ae982a48628cca24d39ce9dfb9251df70b2ea569
Radovan Bast <bast@users.noreply.github.com>
2017-04-04 11:16:56 +0200

    rm unused c code

--- distmesh/src/distance_functions.c
+++ distmesh/src/distance_functions.c
@@ -5,19 +5,8 @@
 
 
 // Quick routines
-static inline double sqr(double x)
-  { return x*x; }
 static inline double length(double x, double y)
   { return sqrt(x*x+y*y); }
-static inline double dot2(double const *x, double const *y)
-  { return x[0]*y[0]+x[1]*y[1]; }
-static inline double fmaxn(double const *x, int n) {
-  double val = x[0]; int i;
-  for (i=1; i<n; i++)
-    if (val < x[i])
-      val = x[i];
-  return val;
-}
 
 
 double dsegment(double x0,double y0,double p1x,double p1y,double p2x,double p2y)
@@ -27,8 +16,8 @@ double dsegment(double x0,double y0,double p1x,double p1y,double p2x,double p2y)
   double w[2]={x0-p1x,
                y0-p1y};
 
-  double c1=v[0]*w[0] + v[1]*w[1]; // dot2(v,w);
-  double c2=v[0]*v[0] + v[1]*v[1]; // dot2(v,v);
+  double c1=v[0]*w[0] + v[1]*w[1];
+  double c2=v[0]*v[0] + v[1]*v[1];
 
   if (c1<=0)
     return length(x0-p1x,y0-p1y);
