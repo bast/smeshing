fcfed5f741d92464378ceb182cfe5b9b65b7e5b7
Radovan Bast <bast@users.noreply.github.com>
2017-04-04 14:53:15 +0200

    add profiling code

--- distmesh/benchmark.py
+++ distmesh/benchmark.py
@@ -1,6 +1,9 @@
 from test import polygon, fstats
 from file_io import write_data
 
+# import cProfile
+# cProfile.run("p, t = polygon('test/benchmark.txt', uniform=True)")
+
 p, t = polygon('test/benchmark.txt', uniform=True)
 fstats(p, t)
 write_data(p, t, 'test/data.txt')
