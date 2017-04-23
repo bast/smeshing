35a880b434b414a6e5c56d620daad908f8e637f2
Radovan Bast <bast@users.noreply.github.com>
2017-04-23 15:22:40 +0200

    rm unused file

--- smeshing/benchmark.py
+++
@@ -1,9 +0,0 @@
-from test import polygon
-from file_io import write_data
-
-# import cProfile
-# cProfile.run("p, t = polygon('test/benchmark.txt', benchmark=True)")
-
-p, t = polygon('test/benchmark.txt', benchmark=True)
-print('num points: {0}, num triangles: {1}'.format(len(p), len(t)))
-write_data(p, t, 'test/data.txt')
