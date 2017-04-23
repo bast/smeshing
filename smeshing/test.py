7807ab6217b1dbd2c9f4d739dfeaed58af9401b5
Radovan Bast <bast@users.noreply.github.com>
2017-04-23 22:14:27 +0200

    add a simplified lofoten example

--- smeshing/test.py
+++ smeshing/test.py
@@ -227,9 +227,17 @@ def test_bench():
         benchmark=True)
 
 
-def test_lofoten():
+def dont_test_lofoten():
     sub(boundary_file_name='data/lofoten/boundary.txt',
         island_file_names=glob.glob('data/lofoten/islands/*.txt'),
+        reference_file_name='test/result-lofoten.txt',
+        skip_test=True,
+        max_num_iterations=5)
+
+
+def dont_test_lofoten_simple():
+    sub(boundary_file_name='data/lofoten/simple-boundary.txt',
+        island_file_names=['data/lofoten/islands/{0}.txt'.format(i) for i in [275, 209, 38, 154, 19, 247, 173, 210, 39, 95]],
         reference_file_name='test/result-lofoten.txt',
         skip_test=True,
         max_num_iterations=5)
