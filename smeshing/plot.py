73ffbad566c160c9cd332f5ea82545c763e1b80e
Radovan Bast <bast@users.noreply.github.com>
2017-05-12 16:09:11 +0200

    when plotting, remove triangles outside bounds

--- smeshing/plot.py
+++ smeshing/plot.py
@@ -17,9 +17,22 @@ def parse_command_line():
 def generate_plot(x, y, triangles, out_file_name):
     import matplotlib.pyplot as plt
 
+    assert len(x) == len(y)
+
+    # remove triangles outside bounds
+    # not sure how they can appear
+    _triangles = []
+    for triple in triangles:
+        keep_triangle = True
+        for t in triple:
+            if not -1 < t < len(x):
+                keep_triangle = False
+        if keep_triangle:
+                _triangles.append(triple)
+
     plt.figure()
     plt.gca().set_aspect('equal')
-    plt.triplot(x, y, triangles, 'g-', markersize=0.2, linewidth=0.2)
+    plt.triplot(x, y, _triangles, 'g-', markersize=0.2, linewidth=0.2)
     plt.savefig(out_file_name, dpi=300)
 
 
