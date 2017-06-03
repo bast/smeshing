a8799fc6e13da12a37c93d992fb8726a55217550
Radovan Bast <bast@users.noreply.github.com>
2017-06-03 18:33:37 +0200

    increase batch size

--- smeshing/main.py
+++ smeshing/main.py
@@ -101,7 +101,7 @@ def create_initial_distribution(r0_max, points_polygon, num_points, within_bound
     while True:
         print('number of initial points {0} out of {1}'.format(count, num_points))
         _points = []
-        for _ in range(10000):
+        for _ in range(min(num_points, 1000000)):
             x = random.uniform(xmin, xmax)
             y = random.uniform(ymin, ymax)
             _points.append([x, y])
