95673e2fc4df159fea8c1ab3d3e0433e4afee530
Radovan Bast <bast@users.noreply.github.com>
2017-07-19 21:22:41 +0200

    switch to lofoten example

--- example.sh
+++ example.sh
@@ -2,7 +2,7 @@
 
 export SMESHING_BUILD_DIR=build
 
-./smesh --boundary=/home/bast/mesh/smeshing/data/fiction/boundary.txt \
-        --islands='/home/bast/mesh/smeshing/data/fiction/island*.txt' \
-        --config=/home/bast/mesh/smeshing/data/fiction/config.yml \
+./smesh --boundary=/home/bast/mesh/smeshing/data/lofoten/boundary.txt \
+        --islands='/home/bast/mesh/smeshing/data/lofoten/islands/*.txt' \
+        --config=/home/bast/mesh/smeshing/data/lofoten/config.yml \
         --output=/home/bast/mesh/smeshing/data.txt
