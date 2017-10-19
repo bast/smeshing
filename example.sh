25abfbee49e5350e44795eeb1fc26bb4f2db4f00
Radovan Bast <bast@users.noreply.github.com>
2017-10-19 17:43:57 +0200

    update example

--- example.sh
+++ example.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./smesh --boundary=/home/bast/tmp/smeshing/data/bear/boundary.txt \
-        --islands='/home/bast/tmp/smeshing/data/bear/island*.txt' \
-        --config=/home/bast/tmp/smeshing/data/bear/config.yml \
+./smesh --boundary=/home/bast/tmp/smeshing/data/happy-bear/boundary.txt \
+        --islands='/home/bast/tmp/smeshing/data/happy-bear/island*.txt' \
+        --config=/home/bast/tmp/smeshing/data/happy-bear/config.yml \
         --output=data.txt
