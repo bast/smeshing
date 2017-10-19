d9cf772b9aa119d1334c27eb20302950c28ed71e
Radovan Bast <bast@users.noreply.github.com>
2017-10-19 12:21:21 +0200

    set a version; fixes #7

--- setup.py
+++ setup.py
@@ -1,14 +1,36 @@
-#!/usr/bin/env python
+from setuptools import setup
+import os
 
-from distutils.core import setup
+_here = os.path.abspath(os.path.dirname(__file__))
 
-setup(name='smeshing',
-      version='0.0.0',
-      description='Simple mesh generator.',
-      author='Radovan Bast',
-      author_email='radovan.bast@uit.no',
-      url='https://github.com/bast/smeshing',
-      scripts=['smesh'],
-      packages=['smeshing'],
-      license='GPL-3.0',
-      install_requires=['cffi', 'click', 'pyyaml'])
+with open(os.path.join(_here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version = {}
+with open(os.path.join(_here, 'smeshing', 'version.py')) as f:
+    exec(f.read(), version)
+
+setup(
+    name='smeshing',
+    version=version['__version__'],
+    description=('Simple mesh generator.'),
+    long_description=long_description,
+    author='Radovan Bast',
+    author_email='radovan.bast@uit.no',
+    url='https://github.com/bast/smeshing',
+    license='GPL-3.0',
+    packages=['smeshing'],
+    install_requires=['cffi', 'click', 'pyyaml'],
+#   install_requires=[
+#       'click==6.7',
+#       'numpy==1.13.1',
+#       'pyyaml==3.12',
+#   ],
+#   scripts=['bin/cooley'],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6'],
+    )
--- smeshing/__init__.py
+++ smeshing/__init__.py
@@ -1,2 +1,7 @@
 from .main import run
 from .file_io import write_data
+from .version import __version__
+
+__all__ = [
+    '__version__',
+]
