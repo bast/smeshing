1a3d690566326092fc5abfa6633b360aeb38928f
Radovan Bast <bast@users.noreply.github.com>
2018-01-30 14:45:31 +0100

    rm outdated flag

--- example.sh
+++ example.sh
@@ -3,5 +3,4 @@
 ./smesh --boundary=/home/bast/tmp/smeshing/data/happy-bear/boundary.txt \
         --islands=/home/bast/tmp/smeshing/data/happy-bear/islands.txt \
         --config=/home/bast/tmp/smeshing/data/happy-bear/config.yml \
-        --resolution_function=/home/bast/tmp/smeshing/data/happy-bear/resolution_function.py \
         --output=data.txt
