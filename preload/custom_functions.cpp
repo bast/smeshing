487fc2f4d18b2be407cceb2a85e8b40050f5e238
Radovan Bast <bast@users.noreply.github.com>
2018-01-30 19:45:21 +0100

    more doc for custom function

--- preload/custom_functions.cpp
+++ preload/custom_functions.cpp
@@ -1,10 +1,18 @@
+// provides std::max
+#include <algorithm>
+
 double h_function(const double coefficients[])
 {
-    return 0.1*coefficients[0];
+    // in this example this is the distance across strait
+    // using view angle of 90 degs
+    return 0.5*coefficients[0];
 }
 
 double g_function(const double distance)
 {
-    double scale_factor = 0.99;
-    return scale_factor * distance;
+    // this is to make sure we do not end up with zero distance
+    // and then try to divide by zero later
+    double result = std::max(0.5, distance);
+
+    return result;
 }
