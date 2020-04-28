3c4f5d249c2f3843d67982c8cc303cb7fb5ad9c1
Radovan Bast <bast@users.noreply.github.com>
2020-04-28 14:14:15 +0200

    fix security warning about pyyaml

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=[
         'cffi==1.11.2',
         'click==6.7',
-        'pyyaml==3.12',
+        'pyyaml>=4.2b1',
         'polygons==0.0.0',
         'delaunay==0.0.0',
         'scipy',
